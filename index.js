dc1f02714002f1140f6b22b8ca4f532d44d8aeb5
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-12 21:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((sentence) => {
       return sentence.split(' ').map((word) => {
       return word[0].toUpperCase() + word.slice(1);
     }).join(' ')
